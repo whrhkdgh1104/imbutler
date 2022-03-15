23c41fd284e0b0a4c6fb93c5ca92c9e1c0ff7ae9
Daniel <whrhkdgh1104@gmail.com>
2022-03-15 15:34:42 +0900

    test commit

--- routes/chat.js
+++ routes/chat.js
@@ -5,14 +5,16 @@ var router = express.Router();
 /* 채팅 접근 */
 router.get('/room', function(req, res, next) {
     if(req.query.to) {
-        if(req.session.user) {
+        if(!req.session.user) {
+            common.alert(res, '로그인이 필요합니다.', '../member/login?callback=/chat/room?to=' + req.query.to);
+        } else if(req.query.to == req.session.user.mb_username) {
+            common.alert(res, '비정상적인 접근입니다.');
+        } else {
             res.render('chat/room', {
                 title: 'Chat',
                 user: req.session.user,
                 to: req.query.to
             });
-        } else {
-            common.alert(res, '로그인이 필요합니다.', '../member/login?callback=/chat/room?to=' + req.query.to);
         }
     } else {
         common.alert(res, '비정상적인 접근입니다.');
